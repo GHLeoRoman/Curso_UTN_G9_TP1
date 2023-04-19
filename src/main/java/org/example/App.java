132f0b763670f1b02c8d144d554ec5047daab952
GHLeoRoman <90406499+GHLeoRoman@users.noreply.github.com>
2023-04-19 19:51:48 -0300

    Entrega 3

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,12 +7,18 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import java.sql.*;
+
+import static conexion.sql.ConectorSQL.DB_URL;
+import static conexion.sql.ConectorSQL.USER;
+import static conexion.sql.ConectorSQL.PASS;
 /**
  * Hello world!
  *
@@ -25,17 +31,27 @@ public class App
     static String pathResultado = "C:\\temp\\resultados.csv";
     static String pathPronostico = "C:\\temp\\pronostico.csv";
 
+    static Connection conexion = null;
+    static Statement consulta = null;
+
+    static String Etapa = "3";
+
     public static void main( String[] args )  throws Exception
     {
-        //System.out.println ("resultados");
-        leeresultados();
-        //System.out.println ("pronosticos");
-        leepronosticos();
-        //System.out.println ("recorre");
-        recorre(pronosticos);
+        if ( Etapa == "2" ) {
+            leeResultadosDesdeArchivo();
+            leePronosticosDesdeArchivo();
+            recorre(pronosticos);
+            }
+            else
+            {
+                leeResultadosDesdeDB();
+                leePronosticosDesdeDB();
+                recorre(pronosticos);
+            }
     }
 
-    private static void leeresultados() {
+    private static void leeResultadosDesdeArchivo() {
         // Leer resultados
         Path ruta  = Paths.get(pathResultado);
         List<String> lineaResultado = null;
@@ -69,7 +85,7 @@ public class App
         }
     }
 
-    private static void leepronosticos(){
+    private static void leePronosticosDesdeArchivo(){
 
 
         Path ruta = Paths.get(pathPronostico);
@@ -96,55 +112,16 @@ public class App
         }
     }
 
-    public static Integer  calculaPuntosII(List<Resultado> resultado, Pronostico pronosticoAux) {
-
-        Optional<Resultado> partidoaux = resultado.stream().filter(a -> a.getEquipo1id().equals(pronosticoAux.getEquipo1id())
-                && a.getEquipo2id().equals(pronosticoAux.getEquipo2id())
-                && a.getRondaid().equals(pronosticoAux.getRondaid())
-        ).findFirst();
-
-
-        if (!partidoaux.isPresent()) {
-            // Excepcion
-            System.out.print("Partido no encontrado");
-        } else
-        {
-            Resultado partido = partidoaux.get();
-            if (pronosticoAux.getGana1().equals("X")) {
-                if (partido.getEquipo1cantidadgoles() > partido.getEquipo2cantidadgoles()) {
-                    // asigno puntos
-                    return 1;
-                }
-            }
-
-            if (pronosticoAux.getGana2().equals("X")) {
-                if (partido.getEquipo1cantidadgoles() < partido.getEquipo2cantidadgoles()) {
-                    // asigno puntos
-                    return 1;
-                }
-
-            }
-
-            if (pronosticoAux.getEmpata().equals("X")) {
-                if (partido.getEquipo1cantidadgoles() == partido.getEquipo2cantidadgoles()) {
-                    // asigno puntos
-                    return 1;
-                }
-            }
-        }
-        return 0;
-    }
-
-    private static void recorre(List<Pronostico> pronostico) {
+   private static void recorre(List<Pronostico> pronostico) {
+        // Totales x Rueda y Persona
         Map<String, Integer> map = pronosticos.stream().collect(
                 Collectors.groupingBy(                            // I want a Map<Integer, Integer>
                         Pronostico::getParticipantenombre,                              // price is the key
                         Collectors.summingInt(Pronostico::getPuntos) ) ); // sum of quantities is the va
-
         System.out.println ("Lista de Puntos Totales ");
-
         map.forEach((key, value) -> System.out.println(key + ":" + value));
 
+        // Totales x Rueda y Persona
         Map<String, Map<String, Integer>> map2 = pronosticos.stream()
                 .collect(
                         Collectors.groupingBy(Pronostico::getRondaid,
@@ -155,6 +132,108 @@ public class App
 
         map2.forEach((key, value) -> System.out.println(key + ":" + value));
 
+
+    }
+
+    private static void leeResultadosDesdeDB() {
+        // Leer resultados
+        try {
+            // Abrir la conexión
+            System.out.println("conectando a la base de datos...");
+            conexion = DriverManager.getConnection(DB_URL, USER, PASS);
+            // Ejecutar una consulta
+            System.out.println("Creating statement...");
+            consulta = conexion.createStatement();
+            String sql;
+            sql = "SELECT Rondaid , Rondanro , Equipo1id , Equipo1nombre ," +
+                    "Equipo1descripcion ,Equipo1cantidadgoles , Equipo2cantidadgoles , " +
+                    "Equipo2id , Equipo2nombre , Equipo2descripcion FROM prode.resultados";
+            //En la variable resultado obtendremos las distintas filas que nos devolvió la base
+            ResultSet rs = consulta.executeQuery(sql);
+
+            // Obtener las distintas filas de la consulta
+            while (rs.next()) {
+                // Obtener el valor de cada columna
+                Resultado resultado = new Resultado();
+                resultado.setRondaid(rs.getString("Rondaid"));
+                resultado.setRondanro(rs.getString("Rondanro"));
+                resultado.setEquipo1id(rs.getString("Equipo1id"));
+                resultado.setEquipo1nombre(rs.getString("Equipo1nombre"));
+                resultado.setEquipo1descripcion(rs.getString("Equipo1descripcion"));
+                resultado.setEquipo1cantidadgoles(rs.getInt("Equipo1cantidadgoles"));
+                resultado.setEquipo2cantidadgoles(rs.getInt("Equipo2cantidadgoles"));
+                resultado.setEquipo2id(rs.getString("Equipo2id"));
+                resultado.setEquipo2nombre(rs.getString("Equipo2nombre"));
+                resultado.setEquipo2descripcion(rs.getString("Equipo2descripcion"));
+                resultados.add(resultado);
+            }
+            // Esto se utiliza par cerrar la conexión con la base de datos
+            rs.close();
+            consulta.close();
+            conexion.close();
+        } catch (SQLException se) {
+            // Execpción ante problemas de conexión
+            se.printStackTrace();
+        } finally {
+            // Esta sentencia es para que ante un problema con la base igual se cierren las conexiones
+            try {
+                if (consulta != null)
+                    consulta.close();
+            } catch (SQLException se2) {
+            }
+            try {
+                if (conexion != null)
+                    conexion.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+    }
+
+    private static void leePronosticosDesdeDB(){
+        try {
+            // Abrir la conexión
+            System.out.println("conectando a la base de datos...");
+            conexion = DriverManager.getConnection(DB_URL, USER, PASS);
+            // Ejecutar una consulta
+            System.out.println("Creating statement...");
+            consulta = conexion.createStatement();
+            String sql;
+            sql = "SELECT Rondaid , participanteid , participantenombre , equipo1id , gana1 , empata , gana2 , FROM prode.resultados";
+            //En la variable resultado obtendremos las distintas filas que nos devolvió la base
+            ResultSet rs = consulta.executeQuery(sql);
+
+            // Obtener las distintas filas de la consulta
+            while (rs.next()) {
+            Pronostico pronostico = new Pronostico(rs.getString("Rondaid"),rs.getString("participanteid"),
+                    ,rs.getString("participantenombre") , rs.getString("equipo1id"),rs.getString("gana1"),
+                    rs.getString("empata"),rs.getString("gana2"),
+                    rs.getString("equipo2id"));
+
+                pronostico.setPuntos(pronostico.calculaPuntos(resultados,pronostico));
+                pronosticos.add(pronostico);
+            }
+            // Esto se utiliza par cerrar la conexión con la base de datos
+            rs.close();
+            consulta.close();
+            conexion.close();
+        } catch (SQLException se) {
+            // Execpción ante problemas de conexión
+            se.printStackTrace();
+        } finally {
+            // Esta sentencia es para que ante un problema con la base igual se cierren las conexiones
+            try {
+                if (consulta != null)
+                    consulta.close();
+            } catch (SQLException se2) {
+            }
+            try {
+                if (conexion != null)
+                    conexion.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
 
     }
 }
